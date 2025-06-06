470a327d793f6eece1fcfc37f37ba6f723282ef2
Deepak kumar <darihatdeepak2@gmail.com>
2025-06-06 21:34:51 +0530

    writes codes in(backend) index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,21 @@
-console.log("Backend server is running...");
-console.log("hi");
-console.log("This is a test message from the backend server.");
-console.log("Listening on port 3000...");
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true
+}
+app.use(cors(corsOptions));
+
+const PORT =  3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
